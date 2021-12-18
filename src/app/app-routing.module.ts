441130e9ceaac9daa87a1285f541208cc8d9e86c
luan-alencar <luanalencar134@gmail.com>
2021-12-18 20:08:11 -0300

    adding categories module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CategoriesModule } from './pages/categories/categories.module';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'categories', loadChildren: () => CategoriesModule }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
